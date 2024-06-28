5370f290163d1eaf21469b389fd2441f3a412a0b
Shubham-07-03 <shubhamkumar98524@gmail.com>
2024-06-28 10:22:24 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,235 @@
+/*
+
+ let score = 33
+
+ console.log(typeof score);
+ console.log(typeof(score));
+
+ let valueInNumber = Number(score)
+ console.log(typeof valueInNumber);
+ console.log(valueInNumber);
+
+ // result=>
+ // number
+ // number
+ // number
+ // 33
+
+*/
+
+
+
+//          N O T E S       //
+
+/**
+ 
+ "33" => 33
+ "33abc" => NaN
+ null => 0
+ undefined => NaN
+ True => 1; False => 0
+ "shubham" => NaN
+
+
+ 1 => true; 0 => false
+ "" => false
+ "shubham" => true
+
+*/
+
+
+
+
+
+
+
+/*
+
+ let score = "33abc"
+
+ console.log(typeof score);
+ console.log(typeof(score));
+
+ let valueInNumber = Number(score)
+ console.log(typeof valueInNumber);
+ console.log(valueInNumber);
+
+ // result=> 
+ // string
+ // string
+ // number
+ // NaN
+
+*/
+
+
+
+
+/*
+
+ let score = null
+
+ console.log(typeof score);
+ console.log(typeof(score));
+
+ let valueInNumber = Number(score)
+ console.log(typeof valueInNumber);
+ console.log(valueInNumber);
+
+ // result
+ // object
+ // object
+ // number
+ // 0
+
+*/
+
+
+
+
+
+/*
+
+ let score = undefined
+
+ console.log(typeof score);
+ console.log(typeof(score));
+
+ let valueInNumber = Number(score)
+ console.log(typeof valueInNumber);
+ console.log(valueInNumber);
+
+ // result
+ // undefined
+ // undefined
+ // number
+ // NaN
+
+*/
+
+
+
+
+
+/*
+
+ let score = true
+
+ console.log(typeof score);
+ console.log(typeof(score));
+
+ let valueInNumber = Number(score)
+ console.log(typeof valueInNumber);
+ console.log(valueInNumber);
+
+ // result
+ // boolean
+ // boolean
+ // number
+ // 1
+
+*/
+
+
+
+/*
+
+ let score = false
+
+ console.log(typeof score);
+ console.log(typeof(score));
+
+ let valueInNumber = Number(score)
+ console.log(typeof valueInNumber);
+ console.log(valueInNumber);
+
+ // result
+ // boolean
+ // boolean
+ // number
+ // 0
+
+*/
+
+
+
+
+/*
+
+ let score = "shubham"
+
+ console.log(typeof score);
+ console.log(typeof(score));
+
+ let valueInNumber = Number(score)
+ console.log(typeof valueInNumber);
+ console.log(valueInNumber);
+
+ // result
+ // string
+ // string
+ // number
+ // NaN
+
+*/
+  
+
+
+
+/*
+ let isLoggedIn = 1  // true
+ let isLoggedIn = "" // false
+ let isLoggedIn = "shubham" // true 
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+*/
+
+   
+
+
+/*
+ let someNumber = 33
+
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);  // 33
+ console.log(typeof stringNumber); // string
+*/
+
+
+
+// *****************  Operations   ************************
+
+/*
+let value = 3
+let negValue = -value  // -3
+// console.log(negValue);
+
+// console.log(2+2); // 4
+// console.log(2-2); // 0
+// console.log(2*2); // 4
+// console.log(2**3); // 8
+// console.log(3/2); // 1.5
+// console.log(3%2); // 1
+*/
+
+
+/*
+
+ let str1 = "hell0"
+ let str2 = " shubham"
+
+ let str3 = str1 + str2 //  hell0 shubham
+ console.log(str3); 
+ console.log("1" + 2); //   12
+ console.log(1 + "2"); //   12
+ console.log("1" + 2 + 2); //  122 
+ console.log(1 + 2 + "2");  //  32 ; [pehle(1+2) ka coversion then string work krega 
+
+console.log( 3 + 4 * 5 % 3 );
+
+console.log(+true); // 1
+console.log(+"" ); // 0
+
+*/
+
+
