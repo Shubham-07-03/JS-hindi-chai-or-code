7123905d4a04ac1229fb591283ad273865d9d2d9
Shubham <shubhamkumar98524@gmail.com>
2024-07-11 04:14:15 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,53 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// const month = 3
+// switch (month) {
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("febuary");
+//         break;
+//     case 3:
+//         console.log("march"); // march 
+//         break;
+//     case 4:
+//         console.log("april");
+//         break;    
+    // default:
+    //     console.log("default case number");
+//         break;
+// }
+
+
+
+const month = "march"
+switch (month) {
+    case "january":
+        console.log("january");
+        break;
+    case "febuary":
+        console.log("febuary");
+        break;
+    case "march":
+        console.log("march"); // march 
+        break;
+    case "april":
+        console.log("april");
+        break;    
+    default:
+        console.log("default case number");
+        break;
+}
+// if break hta de than
+// ek baar jab case match hota hai toh uske sara code execute ho jata hai except default
+
+
