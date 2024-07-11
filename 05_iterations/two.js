eb44f74b20ace445221322b373086f1ecd4bd070
Shubham <shubhamkumar98524@gmail.com>
2024-07-11 10:48:15 +0000

    loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,39 @@
+// While loop  
+
+let index = 1
+while (index < 10) {
+    // console.log(`Value of index is ${index}`); // Value of index is 1 , 3, 5, 7, 9 
+    index = index + 2
+}
+
+
+
+let myArray = ['ironman', 'spiderman', "thor"]
+
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`); // Value is ironman, spiderman , thor 
+    arr++
+    // arr = arr + 1
+}
+
+
+
+// +++++++   do-while loop   +++++++  \\    
+
+// let score = 1
+
+// do {
+//     console.log(`Score is ${score}`); // Score is 1 ,,,,Score is 10
+//     score++    
+// } while (score <= 10);
+
+
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`); // Score is 11
+    score++    
+} while (score <= 10);
+
