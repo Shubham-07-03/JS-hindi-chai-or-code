b0b43723b360655a86766d52f38a21073ccfd6cb
Shubham <shubhamkumar98524@gmail.com>
2024-08-21 14:23:54 +0000

    bind keyword in javascript

---
+++ 10_classes_and_oop/inheritance.js
@@ -0,0 +1,35 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`USERNAME is ${this.username}`);
+    }
+}
+
+class Teacher extends User{
+    constructor(username, email, password){
+        super(username)
+        this.email = email
+        this.password = password
+    }
+
+    addCourse(){
+        console.log(`A new course was added by ${this.username}`);
+    }
+}
+
+const chai = new Teacher("chai", "chai@teacher.com", "123")
+
+// chai.addCourse() // A new course was added by chai
+
+
+chai.logMe()
+const masalaChai = new User("masalaChai")
+masalaChai.logMe()
+
+// console.log(chai === masalaChai); // false 
+// console.log(chai === Teacher); // false 
+console.log(chai instanceof Teacher); // true 
+console.log(chai instanceof User); // true 
