dfffd5aa3dd559a341aaf1b09269beea62a9e280
Shubham <shubhamkumar98524@gmail.com>
2024-09-02 03:26:39 +0000

    Wrapping up js series

---
+++ 10_classes_and_oop/object_get_set.js
@@ -0,0 +1,17 @@
+const User = {
+    _email: 's@sk.com',
+    _password: "abc",
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+
+    set email(value){
+        this._email = value
+    }
+}
+
+const tea = Object.create(User)
+console.log(tea.email);
+console.log(tea.password);
+
